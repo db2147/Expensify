c19c50764e9e0a6e55eb21b272e34ce169971a54
KrisB <david.bierma@gmail.com>
2018-06-14 13:32:01 -0500

    add 404 path, import Link from react-router-dom

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import './styles/styles.scss';
 
-const ExpenseDashboardPage = () => ( 
+const ExpenseDashboardPage = () => (// creating our stateless functional components 
     <div>
       This is from my dashboard component
     </div>
@@ -27,21 +27,41 @@ const HelpPage = () => (
     </div>
 );
 
+const NotFoundPage = () => (// our error page; we use link which enables us to click without a full page refresh 
+    <div>
+      404! - <Link to="/">Go home</Link> 
+    </div>
+);
 
-const routes = (// telling react that whenever we match this page ('/'), we should render this component
+const Header = () => ( // we want to render header on each page
+    <header>
+      <h1>Expensify</h1>
+        <Link to="/">Dashboard</Link>
+        <Link to="/create">Add Expense</Link>
+        <Link to="/edit">Edit Expense</Link>
+        <Link to="/help">Help</Link>
+    </header>
+);
+
+const routes = (// adding the Header component on each page
     <BrowserRouter>
         <div>
-        <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
+          <Header />
+          <Switch>
+            <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+            <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
         </div>
+        
     </BrowserRouter>
 );
 
 
 
-
+// telling react to render our routes, which we have set above
 ReactDOM.render( routes,  document.getElementById('app'));
     
  
